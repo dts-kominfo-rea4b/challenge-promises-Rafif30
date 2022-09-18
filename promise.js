d3d708d9dbc3e2ec269257d6d66ff73ff748d3be
Rafif30 <imamrafif25@gmail.com>
2022-09-18 17:24:12 +0700

    promise function

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  try {
+    const firstPromise = await promiseTheaterIXX();
+    const secondPromise = await promiseTheaterVGC();
+    const output = [...firstPromise, ...secondPromise].reduce((acc, cur) => {
+      if (cur.hasil === emotion) {
+        acc += 1;
+      }
+      return acc;
+    }, 0);
+    return output;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
